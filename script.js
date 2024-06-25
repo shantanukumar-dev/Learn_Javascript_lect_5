c21fa5115c9b0925ab20427cdaac6dbb73c3a568
Shantanu Kumar Mahanta <121172938+shantanukumar-dev@users.noreply.github.com>
2024-06-25 17:54:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+//1.ARITHMETIC OPERATOR
+let a = 10;
+let b = 5;
+console.log(a + b);//Addition operator
+
+console.log(a - b);//Subtraction operator
+
+console.log(a * b);//Multiplication operator
+
+console.log(a / b);//Division oprerator
+
+console.log(a % b);//Modulus Operator
+
+console.log(a ** b);//a to the power b
+
+//2.INCEMENT AND DECREMENT OPERATOR:-
+
+// 1.Increment operator is used in two Form.
+//          1.Pre incement
+
+let c = 1;
+console.log(++c);//2
+console.log(c);//2
+
+//  2.post Increment
+
+let d = 1;
+console.log(d++);//1
+console.log(d);//2
+
+
+// 2.Decrement operator is used in two Form.
+// 1.Predecrement
+let a1 = 5;
+console.log(--a1);//4
+console.log(a1);//4
+
+// 2.postdecrement
+let a2 = 5;
+console.log(a2--);//5
+console.log(a2);//4
+
+//3.COMPARISION OPERATOR
+
+let b1 = 10;
+let b2 = 20;
+console.log(b1 == b2);//equals to
+
+console.log(b1 != b2);//Not equals to
+
+console.log(b1 > b2);//greater than
+
+console.log(b1 < b2);//smaller than
+
+console.log(b1 >= b2);//greater than equals to
+
+console.log(b1 <= b2);//smaller than equals to
+
+//FOR EXAMPLE:-
+const a3 = "6";
+const b3 = 6;
+console.log(a3 === b3);//check both datatypes and value
+console.log(a3 !== b3);//check both datatypes and value
+
+const a4 = true;
+const b4 = 1;
+console.log(a4 === b4);
+console.log(a4 !== b4);
+
+
+// 4.logical operator
+
+// There are three types of logical operator are there
+// 1.LOGICAL OR (||) :- F F=F otherwise true
+console.log(4 < 3 || 3 < 2);//false
+console.log(4 < 3 || 3 < 5);//true
+// 2.LOGICAL AND(&&) :- T T=T otherwise false
+console.log(2 > 1 && 6 > 5);
+console.log(2 > 1 && 4> 5);
+
+// 3.LOGICAL NOT (!):- T=F AND F=T
+console.log(!true);
+console.log(!false);
+
+
+
+
+
+
+
+
+
+
